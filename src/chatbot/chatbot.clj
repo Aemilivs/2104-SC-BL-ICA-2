0ff0c68f09c1bc7ecb5b4b220d89215c9b9b3eac
Vadim Emelin <vemelin@sessionm.com>
2022-01-20 18:29:53 +0100

    Integrate salutation into the chatbot flow.

--- src/chatbot/chatbot.clj
+++ src/chatbot/chatbot.clj
@@ -11,7 +11,7 @@
     [tree]
     (
         format
-            "Hello! I am a chatbot designed to help you in identifying %s.\nPlease answer to the questions with 'yes' and 'no', so that I can give you the most precise answer."
+            "I am a chatbot designed to help you in identifying %s.\nPlease answer to the questions with 'yes' and 'no', so that I can give you the most precise answer."
             (
                 get
                     tree
@@ -21,22 +21,20 @@
 )
 
 (
-	defn greed
+	defn greet
   	"Return user name"
   	[]
   	(
-    println "Hello! What is your name?"
+        println "Hello! What is your name?"
   	)
   	(
     let 
     [
-     input (
-            read-line
-            )
-     ]
-     (
-      println "Hello, " input
-      )
+        input (read-line)
+    ]
+    (
+        println "Welcome, " input "!"
+    )
     )
 )
 
@@ -101,6 +99,9 @@
         ]
         (
             do
+            (
+                greet
+            )
             (
                 println
                 (
