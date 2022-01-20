afa800ad280732b34563361661ae3378a5fcd4cb
Vadim Emelin <vemelin@sessionm.com>
2022-01-20 19:52:45 +0100

    Integrate recognition of words from given lists.

---
+++ src/chatbot/data.clj
@@ -0,0 +1,31 @@
+(ns chatbot.data)
+
+(
+    def positive-responses
+    [
+        "yes",
+        "yup",
+        "yep",
+        "sure",
+        "positive",
+        "affirmative"
+    ]
+)
+
+(
+    def negative-responses
+    [
+        "no",
+        "nope",
+        "negative"
+    ]
+)
+
+(
+    def exit-words
+    [
+        "bye",
+        "exit",
+        "quit"
+    ]
+)
